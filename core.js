c6743757ce076d8467bfc431cf5ed74d241f8922
Juan J <juanmagri@outlook.es>
2021-03-27 15:06:52 -0500

    Promises: async practice

--- core.js
+++ core.js
@@ -1,22 +1,22 @@
-let API = "https://api.exchangeratesapi.io/latest";
+const API = "https://api.exchangeratesapi.io/latest";
 
-const getData = (URL_API, callback) => {
-   const req = new XMLHttpRequest();
-   req.open("GET", URL_API, true);
-   req.onreadystatechange = (eve) => {
-      if (req.readyState === 4) {
-         if (req.status === 200) {
-            callback(null, JSON.parse(req.responseText))
-         } else {
-            let err = new Error("Whooops!");
-            return callback(err, null);
+const getData = (URL_API) => {
+   return new Promise((resolve, reject) => {
+      let req = new XMLHttpRequest();
+      req.open("GET", URL_API, true);
+      req.onreadystatechange = () => {
+         if (req.readyState === 4) {
+            req.status = 200
+               ? resolve(JSON.parse(req.responseText))
+               : reject(new Error("Oops, error " + req.status));
          }
       }
-   }
-   req.send();
+      req.send();
+   })
 }
 
-getData(API, function(error1, data1) {
-   if (error1) return console.error(error1)
-   console.log(data1.rates.USD)
-});
+getData(API)
+   .then(response1 => {
+      console.log(response1.rates.USD);
+   })
+   .catch(err => console.error(err))
