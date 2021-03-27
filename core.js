3281e495b53ca20a1874fd31f1d952ba280b8783
Juan J <juanmagri@outlook.es>
2021-03-27 14:39:29 -0500

    Callbacks: async practice

--- core.js
+++ core.js
@@ -1,1 +1,22 @@
 let API = "https://api.exchangeratesapi.io/latest";
+
+const getData = (URL_API, callback) => {
+   const req = new XMLHttpRequest();
+   req.open("GET", URL_API, true);
+   req.onreadystatechange = (eve) => {
+      if (req.readyState === 4) {
+         if (req.status === 200) {
+            callback(null, JSON.parse(req.responseText))
+         } else {
+            let err = new Error("Whooops!");
+            return callback(err, null);
+         }
+      }
+   }
+   req.send();
+}
+
+getData(API, function(error1, data1) {
+   if (error1) return console.error(error1)
+   console.log(data1.rates.USD)
+});
