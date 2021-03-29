4e0681935a9f56bd1aa1e92137d45c9e2c225fb5
Juan J <juanmagri@outlook.es>
2021-03-29 10:47:35 -0500

    This version helps getting the currency values dynamically

--- core.js
+++ core.js
@@ -1,35 +1,5 @@
-// Converter Script //
-let convert = document.querySelector("#converter");
-let amount = document.querySelector("#amount");
-let from = document.querySelector("#currency-from");
-let to = document.querySelector("#currency-to");
-let fromValue, toValue, amountSubmitted, toVar;
-
-fromValue = "EUR";
-toValue = "USD";
-
-const converting = () => {
-   fromValue = from.value;
-   toValue = to.value;
-   amountSubmitted = amount.value;
-   toVar = eval(toValue);
-   if (fromValue === "EUR") {
-      exchange.innerHTML = parseFloat((toVar * amountSubmitted).toFixed(6));   //It firstly converts the string into a variable (toVar), then multiplies it by the input value with maximum 6 decimal places and lastly parseFloat is used not to show unnecessary zeros.
-   }
-}
-
-amount.addEventListener("keyup", (eve) => {
-   if (eve.keyCode === 13) {
-      convert.click();
-   }
-})
-convert.addEventListener("click", converting);
-
-// /Converter Script //
-
-// API requests //
+// API //
 const API = "https://api.exchangeratesapi.io/latest";
-let exchange = document.querySelector("#exchanged");
 
 const getData = (URL_API) => {
    return new Promise((resolve, reject) => {
@@ -45,18 +15,41 @@ const getData = (URL_API) => {
       req.send();
    })
 }
+// /API //
 
-getData(API)
+// Converter Script //
+let convert = document.querySelector("#converter");
+let amount = document.querySelector("#amount");
+let from = document.querySelector("#currency-from");
+let to = document.querySelector("#currency-to");
+let exchange = document.querySelector("#exchanged");
+let fromValue, toValue, amountSubmitted, toVar, a, AAA;
+
+toValue = to.value;
+
+const converting = () => {
+   fromValue = from.value;
+   toValue = to.value;
+   amountSubmitted = amount.value;
+   a = `response1.rates.`+toValue;
+
+   exchanged.classList.remove("d-none");
+
+   getData(API)
    .then(response1 => {
-      USD = response1.rates.USD;    EUR = response1.rates.EUR;    JPY = response1.rates.JPY;
-      AUD = response1.rates.AUD;    CAD = response1.rates.CAD;    CHF = response1.rates.CHF;
-      CNY = response1.rates.CNY;    HKD = response1.rates.HKD;    NZD = response1.rates.NZD;
-      SEK = response1.rates.SEK;    KRW = response1.rates.KRW;    SGD = response1.rates.SGD;
-      NOK = response1.rates.NOK;    MXN = response1.rates.MXN;    INR = response1.rates.INR;
-      RUB = response1.rates.RUB;    ZAR = response1.rates.ZAR;    TRY = response1.rates.TRY;
-      BRL = response1.rates.BRL;
-      exchange.innerHTML = USD;
+      AAA = eval(a);
+      if (fromValue === "EUR") {
+         exchange.innerHTML = parseFloat((AAA * amountSubmitted).toFixed(6));   //It firstly converts the string into a variable (toVar), then multiplies it by the input value with maximum 6 decimal places and lastly parseFloat is used not to show unnecessary zeros.
+      }
    })
    .catch(err => console.error(err))
+}
 
-// /API requests //
+amount.addEventListener("keyup", (eve) => {
+   if (eve.keyCode === 13) {
+      convert.click();
+   }
+})
+convert.addEventListener("click", converting);
+
+// /Converter Script //
