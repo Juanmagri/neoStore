52513266b2cf2e314155fad1ffe8aba2883ed624
Juan J <juanmagri@outlook.es>
2021-03-29 13:21:06 -0500

    Based on the experimental setup here the currency converter is fully working

--- core.js
+++ core.js
@@ -23,7 +23,7 @@ let amount = document.querySelector("#amount");
 let from = document.querySelector("#currency-from");
 let to = document.querySelector("#currency-to");
 let exchange = document.querySelector("#exchanged");
-let fromValue, toValue, amountSubmitted, toVar, a, AAA;
+let fromValue, toValue, amountSubmitted, toVar, valueInEUR, AAA, BBB;
 
 toValue = to.value;
 
@@ -31,18 +31,24 @@ const converting = () => {
    fromValue = from.value;
    toValue = to.value;
    amountSubmitted = amount.value;
-   a = `response1.rates.`+toValue;
-
-   exchanged.classList.remove("d-none");
+   valueInEUR1 = `response1.rates.`+fromValue;
+   valueInEUR2 = `response1.rates.`+toValue;
 
    getData(API)
    .then(response1 => {
-      AAA = eval(a);
+      AAA = eval(valueInEUR1);
+      BBB = eval(valueInEUR2);
       if (fromValue === "EUR") {
-         exchange.innerHTML = parseFloat((AAA * amountSubmitted).toFixed(6));   //It firstly converts the string into a variable (toVar), then multiplies it by the input value with maximum 6 decimal places and lastly parseFloat is used not to show unnecessary zeros.
+         exchange.innerHTML = parseFloat((BBB * amountSubmitted).toFixed(3));   //It firstly converts the string into a variable (toVar), then multiplies it by the input value with maximum 6 decimal places and lastly parseFloat is used not to show unnecessary zeros.
+      } else if (toValue === "EUR") {
+         exchange.innerHTML = parseFloat((1/(AAA)).toFixed(3));
+      } else {
+         exchange.innerHTML = parseFloat((BBB / eval(valueInEUR1)).toFixed(3));
       }
    })
    .catch(err => console.error(err))
+
+   exchanged.classList.remove("d-none");
 }
 
 amount.addEventListener("keyup", (eve) => {
